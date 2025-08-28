561f7e7d03e63f1dadb358d570e65846a0f6282c
chandana-2101 <chandanamurashilli@gmail.com>
2025-08-28 20:16:00 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,4 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 //app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-module.exports = app;
+export default app;
